a162dfe0f5410e175a20dfb7dfca130e28e62110
AkibaSummer <oopsvpwowlq@gmail.com>
2018-09-11 11:17:06 +0800

    update operator

--- Computational_Geometry.c
+++ Computational_Geometry.c
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const double eps=1e-6;
+const double eps=1e-7;
 const double Pi=acos(-1);
 typedef vector<int> Poly;
 
@@ -19,10 +19,13 @@ struct Point{ // 点类型
     Point operator - (Point b){ // 对应坐标减
         return Point(x-b.x,y-b.y);
     }
-    Point operator * (double b){ // 对应坐标除以常数
+    Point operator * (double b){ // 数乘
         return Point(x*b,y*b);
     }
-    Point operator / (double b){ // 对应坐标除以常数
+    double operator * (Point b){ // 点乘
+        return x*b.x+y*b.y;
+    }
+    Point operator / (double b){ // 数除
         return Point(x/b,y/b);
     }
     double operator ^ (Point b){ // 叉乘
@@ -50,32 +53,52 @@ struct Line{ // 线段类型
         c=p1.x*p2.y-p2.x*p1.y;
         angle=atan2(p2.y-p1.y,p2.x-p1.x);
     }
+    inline Point vec(){
+        return p1-p2;
+    }
+    inline bool operator || (Line l2){ // 判断两直线是否平行
+        return (dcmp(a*l2.b-b*l2.a)==0);
+    }
+    inline bool operator == (Line l2){ // 判断两直线是否相等
+        if (!(*this||l2)) return 0;
+        else return (dcmp(a*l2.c-c*l2.a)==0 &&
+            dcmp(b*l2.c-c*l2.b)==0);
+    }
 };
 
-inline double Dot(Point a,Point b){ // 点乘
-    return a.x*b.x+a.y*b.y;
-}
-
-inline double Cross(Point a,Point b){ // 叉乘
-    return a.x*b.y-a.y*b.x;
-}
-
 inline double Length(Point a){ // 线段长度（重载
-    return Dot(a,a);
+    return a*a;
 }
 
 inline double Length(Line a){ // 线段长度（重载
     return Length(a.p1-a.p2);
 }
 
+inline double Length(Point a,Point b){ // 两点之间的距离
+    return Length(a-b);
+}
+
 inline double Angle(Point a,Point b){ // 计算角度
-    return acos(Dot(a,b)/Length(a)/Length(b));
+    return acos(a*b/Length(a)/Length(b));
+}
+
+inline double Angle(Line a,Line b){
+    return Angle(a.vec(),b.vec());
 }
 
 inline double Area2(Point a,Point b,Point c){// 三角形面积两倍，平行四边形面积
-    return Cross(b-a,c-a);
+    return (b-a)^(c-a);
 }
 
+inline Point SymPoint(Point p,Line l) {  // 求二维平面上点p关于直线p1p2的对称点
+    Point result;
+    double a=l.p2.x-l.p1.x;
+    double b=l.p2.y-l.p1.y;
+    double t=((p.x-l.p1.x)*a+(p.y-l.p1.y)*b)/(a*a+b*b);
+    result.x=2*l.p1.x+2*a*t-p.x;
+    result.y=2*l.p1.y+2*b*t-p.y;
+    return result;
+}
 // 最小圆覆盖
 
 // 最近点对最远点对
